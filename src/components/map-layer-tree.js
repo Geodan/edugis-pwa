e5718d59262d8b968e14f93dd685b09fd0bf48f6
Anne Blankert <anne.blankert@geodan.nl>
2018-11-12 17:15:37 +0100

    added radio and checkboxes

--- src/components/map-layer-tree.js
+++ src/components/map-layer-tree.js
@@ -30,77 +30,118 @@ class MapLayerTree extends LitElement {
     e.stopPropagation();
     this.updates++;
   }
-  handleClick(e, node) {
-    let input;
-    switch (e.target.tagName) {
-      case "LI":
-        input = e.target.querySelector("input");
-        if (input && input.type) {
-          if (input.type == "radio") {
-            input.checked = true;
-          } else {
-            input.checked = !input.checked;
-          }
+  toggleCheck(id) {
+    this.toggleNodeInList(this.nodelist, id, false);
+  }
+  toggleNodeInList(list, id, radio) {
+    if (radio) {
+      if (list.find(item=>item.id === id)) {
+        list.forEach(item=>item.checked=false);
+      }
+    }
+    list.forEach(item=>{
+      if (item.id === id) {
+        item.checked = !item.checked;
+        this.updates++;
+      } else {
+        if (item.sublayers) {
+          this.toggleNodeInList(item.sublayers, id, item.type === "radio");
         }
-        break;
-      case "LABEL":
-        input = e.target.parentNode.querySelector("input");
-        break;
-      case "INPUT":
-        input = e.target;
-        break;
-    }
+      }
+    })
+  }
+  handleClick(e, node) {
+    const input = e.currentTarget.querySelector("div");        
     if (input) {
-      node.checked = input.checked;
+      const id = input.getAttribute("id");
+      this.toggleCheck(id);    
+      e.stopPropagation();
     }
-    e.stopPropagation();
   }
   renderTree(nodeList, opened, radio, groupname) {
-    return html`<ul class="${opened?'':'closed'}">${nodeList.map(node=>{
+    return html`
+      <ul class="${opened?'':'closed'}">${nodeList.map(node=>{
         if (node.sublayers){
-          return html`<li @click="${e=>this.toggleOpen(e, node)}">${node.title}<span class="${node.opened?'arrow-down opened':'arrow-down'}"></span>${this.renderTree(node.sublayers, node.opened, node.type === "radio", node.id)}</li>`
+          return html`<li @click="${e=>this.toggleOpen(e, node)}">
+            ${node.title}
+            <span class="${node.opened?'arrow-down opened':'arrow-down'}"></span>
+            ${this.renderTree(node.sublayers, node.opened, node.type === "radio", node.id)}</li>`
         } else {
-          return html`<li class="data" @click="${(e)=>{this.handleClick(e, node)}}"><input ?checked="${node.checked}" name="${radio?groupname:node.id}" type="${radio?'radio':'checkbox'}" value="${node.id}" id="${node.id}"><label for="${node.id}">${node.title}</label></li>`;
+          return html`<li class="data" @click="${(e)=>{this.handleClick(e, node)}}">
+            <div class="${radio?(node.checked?'radio-on':'radio-off'):(node.checked?'check-on':'check-off')}" name="${radio?groupname:node.id}" value="${node.id}" id="${node.id}"></div>
+            <span class="label">${node.title}</span>
+          </li>`;
         }
     })}</ul>`;
   }
   render() {
     return html`<style>
-        ul {
-                list-style-type: none;
-                padding-left: 10px;
-            }
-            li {
-                border-bottom: 1px solid gray;
-                cursor: pointer;
-            }
-            li:last-child {
-                border-bottom: none;
-            }
-            .arrow-down {
-                border-style: solid;
-                border-width: 1px 1px 0 0;
-                content: '';
-                display: inline-block;
-                height: 8px;
-                position: absolute;
-                right: 20px;
-                left: auto;
-                -ms-transform: rotate(45deg);
-                -webkit-transform: rotate(45deg);
-                transform: rotate(45deg);
-                margin-top: 4px;
-                vertical-align: top;
-                width: 8px;
-                border-color: #555;
-            }
-            .opened {
-                transform: rotate(133deg);
-            }
-            .closed {
-                display: none;
-            }
-        </style>
+      ul {
+        list-style-type: none;
+        padding-left: 10px;
+      }
+      li {
+        border-bottom: 1px solid gray;
+        cursor: pointer;
+        line-height: 1.8em;
+      }
+      li:last-child {
+        border-bottom: none;
+      }
+      .arrow-down {
+        border-style: solid;
+        border-width: 1px 1px 0 0;
+        content: '';
+        display: inline-block;
+        height: 8px;
+        position: absolute;
+        right: 20px;
+        left: auto;
+        -ms-transform: rotate(45deg);
+        -webkit-transform: rotate(45deg);
+        transform: rotate(45deg);
+        margin-top: 4px;
+        vertical-align: top;
+        width: 8px;
+        border-color: #555;
+      }
+      .opened {
+        transform: rotate(133deg);
+      }
+      .closed {
+        display: none;
+      }
+      .radio-on {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        vertical-align: middle;
+        background: url('${this.baseURI}/images/checkradio.png') 0 0;
+      }
+      .radio-off {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        vertical-align: middle;
+        background: url('${this.baseURI}/images/checkradio.png') 0 20px;
+      }
+      .check-on {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        vertical-align: middle;
+        background: url('${this.baseURI}/images/checkradio.png') 20px 20px;
+      }
+      .check-off {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        vertical-align: middle;
+        background: url('${this.baseURI}/images/checkradio.png') 20px 0px;
+      }
+      .label {
+        vertical-align: middle;
+      }
     </style>
     ${this.renderTree(this.nodelist, true)}`;
   }
