31592d711fdc9e7e16123757da664dd272745241
Anne Blankert <anne.blankert@geodan.nl>
2018-12-23 17:00:11 +0100

    add module css files for ms-edge

---
+++ src/components/folder-icon.css.js
@@ -0,0 +1,22 @@
+import {html} from '@polymer/lit-element'
+
+export const foldercss = html`
+<style>
+.folder-icon {
+    display: inline-block;
+    height: 12px;
+    width: 16px;
+    margin-left: 2px;
+}
+.folder-sheet {
+    height: 10px;
+    width: 16px;
+    background-color:#888;
+}
+.folder-tab {
+    height: 2px;
+    width: 6px;
+    border-radius: 1px 4px 0 0;
+    background-color: #888;
+}
+</style>`;
