b254ed0ecd05e1e73691d2280a4b4f654811dfa6
Anne Blankert <anne.blankert@geodan.nl>
2018-12-24 01:37:44 +0100

    use tinycolor for all color conversions

--- src/utils/mbox-style-parse.js
+++ src/utils/mbox-style-parse.js
@@ -35,71 +35,12 @@ class MBStyleParser
     }
     return result;
   }
-  hslToRgb(h, s, l){
-    var r, g, b;
-  
-    s = s / 100;
-    l = l / 100;
-    if(s == 0){
-        r = g = b = l; // achromatic
-    }else{
-        var hue2rgb = function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        }
-  
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
-    }
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-  }
-  sevenCharHexColor(colorString) {
-    // colorString expected to be either #xyz or #xxyyzz
-    if (colorString.length === 4) {
-      colorString = colorString.split('').map((char,index)=>index?char+char:char).join('');
-    }
-    return colorString;
-  }
   colorToHex(propertyValue) {
     if (typeof propertyValue !== "string") {
       return propertyValue;
     }
-    let colorString = propertyValue.replace(' ', '');
-    if (colorString.startsWith('#')) {
-      return this.sevenCharHexColor(colorString);
-    }
-    if (colorString.startsWith('hsl(')) {
-      colorString = colorString.substring(4).split(',').map(value=>parseFloat(value));
-      const rgb = hslToRgb(colorString[0], colorString[1], colorString[2]);
-      return "#" + rgb[0].toString(16) + rgb[1].toString(16) + rgb[2].toString(16);
-    }
-    if (colorString.startsWith('hsla(')) {
-      colorString = colorString.substring(5).split(',').map(value=>parseFloat(value));
-      const rgb = hslToRgb(colorString[0], colorString[1], colorString[2]);
-      return "#" + rgb[0].toString(16) + rgb[1].toString(16) + rgb[2].toString(16);
-    }
-    if (colorString.startsWith('rgba(')) {
-      const rgb = colorString.substring(5).split(',').map(value=>parseFloat(value));
-      return "#" + rgb[0].toString(16) + rgb[1].toString(16) + rgb[2].toString(16);
-    }
-    if (colorString.startsWith('rgb(')) {
-      const rgb = colorString.substring(4).split(',').map(value=>parseFloat(value));
-      return "#" + rgb[0].toString(16) + rgb[1].toString(16) + rgb[2].toString(16);
-    }
-    if (tinycolor.names[colorString]) {
-      const hexColor = tinycolor.names[colorString];
-      if (hexColor) {
-        return this.sevenCharHexColor('#' + hexColor);
-      }
-    }
-    return propertyValue;
+    const color = tinycolor(propertyValue);
+    return color.toHexString();
   }
   styleArrayToItems(styleArray, paintPropertyName)
   {
