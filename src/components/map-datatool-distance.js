2d1c1858eabe9d3062582f27c6c5daa4037e80c6
Anne Blankert <anne.blankert@geodan.nl>
2019-04-02 11:51:29 +0200

    add Jsts to calculate distance between non-points

--- src/components/map-datatool-distance.js
+++ src/components/map-datatool-distance.js
@@ -54,33 +54,72 @@ class MapDataToolDistance extends LitElement {
     this.buttonEnabled = (selections.length === 2 && (selections[0].value != selections[1].value))
   }
   _getFeatures(layerid) {
-    const layer = this.map.getLayer(layerid);
-    const source = this.map.getSource(layer.source);
-    switch (source.type) {
-      case "geojson":
-        return source.serialize().data;
-      case "vector":
-        //const result = this.map.querySourceFeatures(layerid, layer.sourceLayer?{sourceLayer:layer.sourceLayer}:undefined);
-        const result = this.map.queryRenderedFeatures({layers:[layerid]});
-        const geojson = {
-          "type": "FeatureCollection",
-          "features": result.map(vectorFeature=>{
-            return {
-              "type": "Feature",
-              "properties": vectorFeature.properties,
-              "geometry": vectorFeature.geometry
-            }
-          })
-        }
-        return geojson;
-      default:
-        console.log(`unhandled source type: ${source.type}`);
-        return [];
-    }
+    return new Promise((resolve, reject)=>{
+      const layer = this.map.getLayer(layerid);
+      const source = this.map.getSource(layer.source);
+      switch (source.type) {
+        case "geojson":
+          const urlOrJson = source.serialize().data;
+          if (typeof urlOrJson === "string") {
+            return fetch(urlOrJson).then(response=>{
+              if (!response.ok) {
+                reject(response.statusText);
+              }
+              return response.json();
+            }).then(json=>{
+              resolve(json);
+            })
+          } else {
+            resolve(urlOrJson)
+          }
+          break;
+        case "vector":
+          //const result = this.map.querySourceFeatures(layerid, layer.sourceLayer?{sourceLayer:layer.sourceLayer}:undefined);
+          const result = this.map.queryRenderedFeatures({layers:[layerid]});
+          const geojson = {
+            "type": "FeatureCollection",
+            "features": result.map(vectorFeature=>{
+              return {
+                "type": "Feature",
+                "properties": vectorFeature.properties,
+                "geometry": vectorFeature.geometry
+              }
+            })
+          }
+          resolve(geojson);
+        default:
+          console.log(`unhandled source type: ${source.type}`);
+          return resolve([]);
+      }
+    })
   }
-  _calculateDistancess(layer1id, layer2id) {
-    const features1 = this._getFeatures(layer1id);
-    const features2 = this._getFeatures(layer2id);
+  _turfDistance(sourceFeatures1, sourceFeatures2) {
+    const distanceGeojson = {
+      "type": "FeatureCollection",
+      "features": []
+    }
+    for (let feature1 of sourceFeatures1.features) {
+      let minDistance = 50000;
+      let resultFeature2;
+      for (let feature2 of sourceFeatures2.features) {
+        const distance = turf.distance(feature1, feature2);
+        
+        if (distance < minDistance) {
+          minDistance = distance;
+          resultFeature2 = feature2;
+        }
+      }
+      const feature = GeoJSON.Feature("LineString");
+      
+      feature.geometry.coordinates = [feature1.geometry.coordinates, resultFeature2.geometry.coordinates];
+      feature.properties.distance = minDistance;
+      distanceGeojson.features.push(feature);
+    }
+    return distanceGeojson;
+  }
+  _jstsDistance(sourceFeatures1, sourceFeatures2) {
+    const features1 = GeoJSON._project(sourceFeatures1, 'EPSG:4326', 'EPSG:3857');
+    const features2 = GeoJSON._project(sourceFeatures2, 'EPSG:4326', 'EPSG:3857');
 
     const distanceGeojson = {
       "type": "FeatureCollection",
@@ -89,26 +128,40 @@ class MapDataToolDistance extends LitElement {
     const geoJSONParser = new GeoJSONParser();
     
     for (let feature1 of features1.features) {
-      let minDistance = 50000;
-      let resultFeature1, resultFeature2;
+      let minDistance = 50000000;
+      let points;
+      const jstGeom1 = geoJSONParser.read(feature1.geometry);
       for (let feature2 of features2.features) {
-        //const jstGeom1 = geoJSONParser.read(feature1);
-        //const jstGeom2 = geoJSONParser.read(feature2);
-        //const distanceOp = new DistanceOp(jstGeom1, jstGeom2);
-        //const distance = distanceOp.distance();
-    
-        const distance = turf.distance(feature1, feature2);
-        
+        const jstGeom2 = geoJSONParser.read(feature2.geometry);
+        const distanceOp = new DistanceOp(jstGeom1, jstGeom2);
+        const distance = distanceOp.distance();    
         if (distance < minDistance) {
           minDistance = distance;
-          resultFeature1 = feature1;
-          resultFeature2 = feature2;
+          points = distanceOp.nearestPoints();
         }
       }
-      const feature = new GeoJSON.Feature("LineString");
-      feature.geometry.coordinates = [resultFeature1.geometry.coordinates, resultFeature2.geometry.coordinates];
-      feature.properties.distance = minDistance;
+      const feature = GeoJSON.Feature("LineString");
+      
+      feature.geometry.coordinates = [[points[0].x, points[0].y], [points[1].x, points[1].y]]
+      feature.properties.distance = minDistance/1000;
       distanceGeojson.features.push(feature);
+    }
+    return GeoJSON._project(distanceGeojson, 'EPSG:3857', 'EPSG:4326')
+  }
+  async _calculateDistances(layer1id, layer2id) {
+    const sourceFeatures1 = await this._getFeatures(layer1id);
+    const sourceFeatures2 = await this._getFeatures(layer2id);
+    let resultFeatures;
+    if (sourceFeatures1.type === 'FeatureCollection' && 
+        sourceFeatures1.features.length && 
+        sourceFeatures1.features[0].geometry.type === 'Point' &&
+        sourceFeatures2.type === 'FeatureCollection' && 
+        sourceFeatures2.features.length &&
+        sourceFeatures2.features[0].geometry.type === 'Point') {
+        // two point layers, calculate distance over the globe
+        resultFeatures = this._turfDistance(sourceFeatures1, sourceFeatures2);
+    } else {
+      resultFeatures = this._jstsDistance(sourceFeatures1, sourceFeatures2);
     }
     const newLayer = {
       id: GeoJSON._uuidv4(),
@@ -118,13 +171,14 @@ class MapDataToolDistance extends LitElement {
       type: "line",
       source : {
         type: "geojson",
-        data: distanceGeojson
+        data: resultFeatures
       },
       paint: {
         "line-color": "black",
         "line-width": 1
       }
     }
+    
     this.dispatchEvent(new CustomEvent('addlayer', {
       detail: newLayer,
       bubbles: true,
@@ -139,7 +193,7 @@ class MapDataToolDistance extends LitElement {
     if (selections.length == 2) {
       const layer1id = selections[0].value;
       const layer2id = selections[1].value;
-      this._calculateDistancess(layer1id, layer2id);
+      this._calculateDistances(layer1id, layer2id);
     }
   }
 }
