a2215786088c3f289bef0527111d670619599b9f
Anne Blankert <anne.blankert@geodan.nl>
2019-11-10 23:59:07 +0100

    fix classcount dropdown values

--- src/components/map/layer/map-layer-config-legend.js
+++ src/components/map/layer/map-layer-config-legend.js
@@ -79,11 +79,11 @@ class ClassificationSettings extends LitElement {
                     <option value="5" ?selected="${this.classCount===5}">5</option>
                     <option value="6" ?selected="${this.classCount===6}">6</option>
                     <option value="7" ?selected="${this.classCount===7}">7</option>
-                    <option value="8" ?selected="${this.classCount===7}">8</option>
-                    <option value="9" ?selected="${this.classCount===7}">9</option>
-                    <option value="10" ?selected="${this.classCount===7}">10</option>
-                    <option value="11" ?selected="${this.classCount===7}">11</option>
-                    <option value="12" ?selected="${this.classCount===7}">12</option>
+                    <option value="8" ?selected="${this.classCount===8}">8</option>
+                    <option value="9" ?selected="${this.classCount===9}">9</option>
+                    <option value="10" ?selected="${this.classCount===10}">10</option>
+                    <option value="11" ?selected="${this.classCount===11}">11</option>
+                    <option value="12" ?selected="${this.classCount===12}">12</option>
                 </base-select></p>
                 <p>Classification methods:<br>
                 <base-button-radio id="classtype" small>
