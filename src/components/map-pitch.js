0a1943a61d203ff302af4f22f15ff33feefba023
Anne Blankert <anne.blankert@geodan.nl>
2021-07-05 14:22:59 +0200

    add info text to 3D tool

--- src/components/map-pitch.js
+++ src/components/map-pitch.js
@@ -49,12 +49,18 @@ class MapPitch extends LitElement {
         padding-bottom: 10px;
         margin-bottom: 10px;
       }
+      .toolpanel {
+        font-size: 14px;
+        padding-top: 8px;
+      }
       </style>
       <div class="padded" style="user-select:none">
-      <div class="heading">Huidige kaarthoek</div>
-      <mwc-button class="edugisblue" ?outlined="${this.pitch!==0}" ?unelevated="${this.pitch===0}" @click="${e=>this.updatePitch(0)}">0&deg;</mwc-button>
-      <mwc-button class="edugisblue" ?outlined="${this.pitch===0 || this.pitch===60}" ?unelevated="${this.pitch!==0 && this.pitch!==60}" @click="${e=>this.updatePitch(this.pitch===0||this.pitch===60?30:this.pitch)}">${this.pitch!==0 && this.pitch!==60?Math.round(this.pitch):30}&deg;</mwc-button>
-      <mwc-button class="edugisblue" ?outlined="${this.pitch!==60}" ?unelevated="${this.pitch===60}" @click="${e=>this.updatePitch(60)}">60&deg;</mwc-button></div>`;          
+        <div class="heading">Huidige kaarthoek</div>
+        <mwc-button class="edugisblue" ?outlined="${this.pitch!==0}" ?unelevated="${this.pitch===0}" @click="${e=>this.updatePitch(0)}">0&deg;</mwc-button>
+        <mwc-button class="edugisblue" ?outlined="${this.pitch===0 || this.pitch===60}" ?unelevated="${this.pitch!==0 && this.pitch!==60}" @click="${e=>this.updatePitch(this.pitch===0||this.pitch===60?30:this.pitch)}">${this.pitch!==0 && this.pitch!==60?Math.round(this.pitch):30}&deg;</mwc-button>
+        <mwc-button class="edugisblue" ?outlined="${this.pitch!==60}" ?unelevated="${this.pitch===60}" @click="${e=>this.updatePitch(60)}">60&deg;</mwc-button>
+        <div class="toolpanel">Kies hierboven een andere kaarthoek<br><i>of</i> gebruik CTRL + muisknop<br><i>of</i> sleep de kompasnaald links onderaan de kaart</div>
+      </div>`;          
   }
   updated() {
     // remove focus from buttons
