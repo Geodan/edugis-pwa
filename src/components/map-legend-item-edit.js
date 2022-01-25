bd981adedaff12c55f751d710098989440dd9f3d
Anne Blankert <anne.blankert@geodan.nl>
2022-01-25 15:01:30 +0100

    move rangeslider css from colorpicker to item-edit

--- src/components/map-legend-item-edit.js
+++ src/components/map-legend-item-edit.js
@@ -32,7 +32,96 @@ class MapLegendItemEdit extends LitElement {
             border: 1px solid gray;
             cursor: pointer;
       }
-      
+      input[type=range] {
+        height: 17px;
+        -webkit-appearance: none;
+        margin: 10px 0;
+        width: 100%;
+        background-color: rgba(0,0,0,0);
+      }
+      input[type=range]:focus {
+        outline: none;
+      }
+      input[type=range]::-webkit-slider-runnable-track {
+        width: 100%;
+        height: 2px;
+        cursor: pointer;
+        animate: 0.2s;
+        box-shadow: 0px 0px 0px #000000;
+        background: #CCCCCC;
+        border-radius: 5px;
+        border: 0px solid #000000;
+      }
+      input[type=range]::-webkit-slider-thumb {
+        box-shadow: 0px 0px 0px #000000;
+        border: 0px solid #2497E3;
+        height: 11px;
+        width: 11px;
+        border-radius: 10px;
+        background: #555;
+        cursor: pointer;
+        -webkit-appearance: none;
+        margin-top: -4.5px;
+      }
+      input[type=range]:focus::-webkit-slider-runnable-track {
+        background: #CCCCCC;
+      }
+      input[type=range]::-moz-range-track {
+        width: 100%;
+        height: 2px;
+        cursor: pointer;
+        animate: 0.2s;
+        box-shadow: 0px 0px 0px #000000;
+        background: #CCCCCC;
+        border-radius: 5px;
+        border: 0px solid #000000;
+      }
+      input[type=range]::-moz-range-thumb {
+        box-shadow: 0px 0px 0px #000000;
+        border: 0px solid #2497E3;
+        height: 11px;
+        width: 11px;
+        border-radius: 10px;
+        background: #555;
+        cursor: pointer;
+      }
+      input[type=range]::-ms-track {
+        width: 100%;
+        height: 2px;
+        cursor: pointer;
+        animate: 0.2s;
+        background: transparent;
+        border-color: transparent;
+        color: transparent;
+      }
+      input[type=range]::-ms-fill-lower {
+        background: #CCCCCC;
+        border: 0px solid #000000;
+        border-radius: 10px;
+        box-shadow: 0px 0px 0px #000000;
+      }
+      input[type=range]::-ms-fill-upper {
+        background: #CCCCCC;
+        border: 0px solid #000000;
+        border-radius: 10px;
+        box-shadow: 0px 0px 0px #000000;
+      }
+      input[type=range]::-ms-thumb {
+        margin-top: 1px;
+        box-shadow: 0px 0px 0px #000000;
+        border: 0px solid #2497E3;
+        height: 11px;
+        width: 11px;
+        border-radius: 10px;
+        background: #555;
+        cursor: pointer;
+      }
+      input[type=range]:focus::-ms-fill-lower {
+        background: #CCCCCC;
+      }
+      input[type=range]:focus::-ms-fill-upper {
+        background: #CCCCCC;
+      }
       `
     }
     static get properties() { 
