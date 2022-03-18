009e9eeeac76ec73f773451e769fd682b1de9b92
Anne Blankert <anne.blankert@geodan.nl>
2022-03-18 23:42:32 +0100

    apply changes by timj

--- src/components/map-search.js
+++ src/components/map-search.js
@@ -1,75 +1,80 @@
-
-import './map-iconbutton';
-import {imageSearchIcon, searchIcon, panoramaWideIcon as areaIcon, showChartIcon as lineIcon, locationOnIcon as pointIcon, closeIcon } from './my-icons';
-import {searchIcon as gmSearchIcon } from '../gm/gm-iconset-svg';
-
+import "./map-iconbutton.js";
+import { imageSearchIcon, searchIcon, panoramaWideIcon as areaIcon, showChartIcon as lineIcon, locationOnIcon as pointIcon, closeIcon } from "./my-icons.js";
+import { searchIcon as gmSearchIcon } from "../gm/gm-iconset-svg.js";
 
 function getIcon(osmtype) {
-  switch(osmtype) {
+  switch (osmtype) {
     case 'relation':
       return areaIcon;
+
     case 'node':
       return pointIcon;
+
     case "way":
       return lineIcon;
+
     default:
       return osmtype;
   }
 }
 
-import {LitElement, html} from 'lit-element';
+import { LitElement, html } from "../../node_modules/lit-element/index.js";
 /**
 * @polymer
 * @extends HTMLElement
 */
+
 class MapSearch extends LitElement {
-  static get properties() { 
-    return { 
+  static get properties() {
+    return {
       info: String,
       resultList: Array,
       viewbox: Array,
       active: Boolean
-    }; 
+    };
   }
+
   constructor() {
-    super();
-    // properties
-    this.info = "zoek plaats of adres (via nominatim)";
+    super(); // properties
+
+    this.info = "Landen, Plaatsen, Gebergten, Rivieren";
     this.resultList = null;
     this.viewbox = undefined;
     this.active = true;
   }
+
   triggerResult() {
     if (this.prevResultList == null && this.resultList == null) {
-        return;
+      return;
     }
+
     this.prevResultList = this.resultList;
-    this.dispatchEvent(
-      new CustomEvent('searchresult',
-        {
-            detail: this.resultList,
-            bubbles: true,
-            composed: true
-        })
-    );
+    this.dispatchEvent(new CustomEvent('searchresult', {
+      detail: this.resultList,
+      bubbles: true,
+      composed: true
+    }));
   }
+
   search(e) {
     const searchText = this.shadowRoot.querySelector('input').value.trim();
+
     if (searchText.length > 1) {
       let url;
+
       if (this.viewbox) {
         url = `https://nominatim.openstreetmap.org/search/${encodeURIComponent(searchText)}?format=json&viewbox=${this.viewbox.join(',')}&bounded=0&polygon_geojson=1&addressdetails=1&limit=15`;
       } else {
         url = `https://nominatim.openstreetmap.org/search/${encodeURIComponent(searchText)}?format=json&polygon_geojson=1&addressdetails=1&limit=15`;
       }
-      fetch(url)
-      .then(response => response.json())
-      .then(data => {
+
+      fetch(url).then(response => response.json()).then(data => {
         this.resultList = data;
         this.triggerResult();
-      })
-    } 
+      });
+    }
   }
+
   keyup(e) {
     if (e.keyCode == 13) {
       this.search(e);
@@ -78,33 +83,37 @@ class MapSearch extends LitElement {
       this.triggerResult();
     }
   }
+
   changed(e) {
     this.resultList = null;
     this.triggerResult();
   }
+
   zoomTo(point, bbox) {
-    this.dispatchEvent(
-      new CustomEvent('searchclick',
-        {
-            detail: {point: point, bbox: [bbox[2],bbox[0],bbox[3],bbox[1]]},
-            bubbles: true,
-            composed: true
-        })
-  );
+    this.dispatchEvent(new CustomEvent('searchclick', {
+      detail: {
+        point: point,
+        bbox: [bbox[2], bbox[0], bbox[3], bbox[1]]
+      },
+      bubbles: true,
+      composed: true
+    }));
   }
-  searchErase(e)
-  {
+
+  searchErase(e) {
     if (this.resultList !== null) {
       this.shadowRoot.querySelector('input').value = "";
       this.resultList = null;
       this.triggerResult();
     }
   }
+
   render() {
     if (!this.active) {
       this.searchErase();
       return html``;
     }
+
     return html`<style>        
         .searchbox {
           width: 100%;
@@ -168,38 +177,29 @@ class MapSearch extends LitElement {
           display: none;
         }
     </style>
-    <div class="searchbox${this.active?'':' hidden'}">
-      <input type="text" placeholder="${this.info}" @keyup="${(e)=>this.keyup(e)}">
-      ${this.active&&this.resultList&&this.resultList.length?html`<i class="erasebutton" @click="${e=>this.searchErase(e)}">${closeIcon}</i>`:''}
-      <span title="zoek" class="searchbutton" @click="${(e)=>this.search(e)}">${searchIcon}</span>
+    <div class="searchbox${this.active ? '' : ' hidden'}">
+      <input type="text" placeholder="${this.info}" @keyup="${e => this.keyup(e)}">
+      ${this.active && this.resultList && this.resultList.length ? html`<i class="erasebutton" @click="${e => this.searchErase(e)}">${closeIcon}</i>` : ''}
+      <span title="zoek" class="searchbutton" @click="${e => this.search(e)}">${searchIcon}</span>
     </div>
-    ${(this.active && this.resultList && this.resultList.length)?
-      html`
+    ${this.active && this.resultList && this.resultList.length ? html`
       <div class="resultlist">
         <ul>
-          ${this.resultList.map(item=>
-            html`
-            <li @click="${e=>this.zoomTo([item.lon,item.lat],item.boundingbox)}">
-                ${item.hasOwnProperty('icon')?
-                  html`
-                  <img src="${item.icon}">`
-                    :
-                  html`${getIcon(item.osm_type)}`}
+          ${this.resultList.map(item => html`
+            <li @click="${e => this.zoomTo([item.lon, item.lat], item.boundingbox)}">
+                ${item.hasOwnProperty('icon') ? html`
+                  <img src="${item.icon}">` : html`${getIcon(item.osm_type)}`}
                 ${item.display_name}
             </li>`)}
         </ul>
-      </div>`
-        :
-      (this.active && (Array.isArray(this.resultList) && this.resultList.length === 0)?
-        html`
+      </div>` : this.active && Array.isArray(this.resultList) && this.resultList.length === 0 ? html`
           <div class="resultlist">
             <ul>
               <li>niets gevonden</li>
             </ul>
-          </div>`
-          :
-        '')}`
+          </div>` : ''}`;
   }
-  
+
 }
+
 customElements.define('map-search', MapSearch);
