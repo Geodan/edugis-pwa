7a8df51cd76931b127953badbff8f972b60f996f
Anne Blankert <anne.blankert@geodan.nl>
2022-12-11 23:24:01 +0100

    add default keys.js to git

---
+++ src/keys.js
@@ -0,0 +1,10 @@
+APIkeys = {
+    mapbox: "pk.your-personal-public-mapbox-key",
+    geodanmaps: "your-personal-public-geodanmaps-key",
+    freetilehosting: "your-personal-freetilehosting-key",
+    bing: "your-personal-bing-key",
+    nextzen: "your-personal-nextzen-key",
+    google: "your-personal-google-key",
+    metoffice: "your-personal-metoffice-key",
+    openweathermap: "your-personal-openweathermap-key"
+}
