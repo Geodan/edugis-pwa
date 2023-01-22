a129f740b23cec245ad9f5c733719aa5dd591d1f
Anne Blankert <anne.blankert@geodan.nl>
2023-01-23 00:05:56 +0100

    remove console.log

--- src/workers/buffer.js
+++ src/workers/buffer.js
@@ -2,7 +2,6 @@ importScripts('https://unpkg.com/@turf/turf@6/turf.min.js');
 //importScripts('../../node_modules/@turf/buffer/dist/es/index.js');
 
 this.onmessage = function(evt) {
-    console.log(evt.data[1]);
     const geojson = evt.data[0];
     const bufsize = evt.data[1];
     const bufferedFeatures = turf.buffer(geojson, bufsize, { units: 'meters' });
