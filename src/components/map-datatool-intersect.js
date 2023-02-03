a912e10b3ccf45c1527bc412ac458f1897df76f9
Anne Blankert <anne.blankert@geodan.nl>
2023-02-03 17:10:41 +0100

    fix updatelayer, add user defined layer name

--- src/components/map-datatool-intersect.js
+++ src/components/map-datatool-intersect.js
@@ -14,7 +14,8 @@ class MapDatatoolIntersect extends LitElement {
           resulttype: {type: String},
           map: {type: Object},
           intersectCount: {type: Number},
-          busyMessage: {type: String}
+          busyMessage: {type: String},
+          outputLayername: {type: String}
         }; 
     }
     constructor() {
@@ -22,6 +23,7 @@ class MapDatatoolIntersect extends LitElement {
         this.resulttype = 'complete';
         this.map = {};
         this.intersectCount = -1;
+        this.outputLayername = "";
         this.worker = new Worker('./src/workers/intersect.js');
         this.worker.onmessage = (e) => this._handleWorkerMessage(e);
         this.busyMessage = '';
@@ -52,6 +54,7 @@ class MapDatatoolIntersect extends LitElement {
             <input type="radio" name="resulttype" value="complete" id="complete" ?checked="${this.resulttype==='complete'}" @change="${(e)=>this._radioChanged(e)}"><label for="complete">Volledig</label><br>
             <input type="radio" name="resulttype" value="intersectonly" id="intersectonly" ?checked="${this.resulttype==='intersectonly'}" @change="${(e)=>this._radioChanged(e)}"><label for="intersectonly">Alleen elementen met een overlap</label><br>
             <input type="radio" name="resulttype" value="nonintersectonly" id="nonintersectonly" ?checked="${this.resulttype==='nonintersectonly'}" @change="${(e)=>this._radioChanged(e)}"><label for="nonintersectonly">Alleen elementen zonder een overlap</label><br>
+            <input type="text" placeholder="naam van uitvoerlaag" name="outputname" ?disabled="${!this.targetLayerid||!this.sourceLayerid}" @keyup="${(e)=>this._outputNameUpdate(e)}" .value="${this.outputLayername}">
             <wc-button class="edugisblue" @click="${e=>this._handleClick(e)}" ?disabled="${!this.buttonEnabled}">Berekenen</wc-button><br>
             ${this.busyMessage !== '' ? html`${this.busyMessage}`:html``}
             ${this.intersectCount > -1 ?html`Aantal elementen: ${this.intersectCount}`:html``}
@@ -67,12 +70,27 @@ class MapDatatoolIntersect extends LitElement {
     _radioChanged(e) {
       this.resulttype = e.target.value;
     }
+    _outputNameUpdate(e) {
+      this.outputLayername = e.target.value;
+      this.buttonEnabled = this.outputLayername.trim().length && this.sourceLayerid && this.targetLayerid;
+    }
     _layerSelected(e) {
       const selections = this.shadowRoot.querySelectorAll('select');
-      this.buttonEnabled = (selections.length === 2 && (selections[0].value != selections[1].value) && (selections[0].value !== '') && selections[1].value !== '');
-      this.sourceLayerid = this.buttonEnabled ? selections[0].value : undefined;
-      this.targetLayerid = this.buttonEnabled ? selections[1].value : undefined;
+      const validLayerSelection = (selections.length === 2 && (selections[0].value != selections[1].value) && (selections[0].value !== '') && selections[1].value !== '');
+      this.buttonEnabled = validLayerSelection && this.outputLayername.trim().length > 0
+      this.sourceLayerid = validLayerSelection ? selections[0].value : undefined;
+      this.targetLayerid = validLayerSelection ? selections[1].value : undefined;
       this.intersectCount = -1;
+      if (this.sourceLayerid && this.targetLayerid) {
+        const outputLayer = this.map.getLayer(this.sourceLayerid+this.targetLayerid);
+        if (outputLayer) {
+          this.outputLayername = outputLayer.metadata.title;
+        } else {
+          this.outputLayername = "";
+        }
+      } else {
+        this.outputLayername = "";
+      }
       this.update();
     }
     _renderLayerList() {
@@ -93,8 +111,8 @@ class MapDatatoolIntersect extends LitElement {
       }
       const resultLayerId = sourceLayerid+targetLayerid;
       if (this.map.getLayer(resultLayerId)) {
-        this.dispatchEvent(new CustomEvent('removeLayer', {
-          detail: resultLayerId,
+        this.dispatchEvent(new CustomEvent('removelayer', {
+          detail: {layerid: resultLayerId},
           bubbles: true,
           composed: true
         }))
@@ -102,7 +120,7 @@ class MapDatatoolIntersect extends LitElement {
       this.resultLayer = {
         id: resultLayerId,
         metadata: {
-          title: "intersect"
+          title: this.outputLayername
         },
         type: sourceLayer.type,
         source: {
@@ -175,7 +193,7 @@ class MapDatatoolIntersect extends LitElement {
         }
         const selections = this.shadowRoot.querySelectorAll('select');
         this.resultLayer.source.data = intersectLayer;
-        this.resultLayer.metadata.abstract = `Deze kaartlaag is gemaakt met de 'intersect'-tool. ${this.intersectCount} elementen uit ${selections[0][selections[0].selectedIndex].text} snijden met de elementen uit ${selections[1][selections[1].selectedIndex].text}`
+        this.resultLayer.metadata.abstract = `Deze kaartlaag is gemaakt met de EduGIS 'intersect'-tool. ${this.intersectCount} elementen uit ${selections[0][selections[0].selectedIndex].text} snijden met de elementen uit ${selections[1][selections[1].selectedIndex].text}`;
         this.dispatchEvent(new CustomEvent('addlayer', {
           detail: this.resultLayer,
           bubbles: true,
