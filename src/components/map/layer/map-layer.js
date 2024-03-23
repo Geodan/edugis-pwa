00a82144db164ef6ed121c0d9e898deb6346079c
Anne Blankert <anne.blankert@geodan.nl>
2024-03-23 16:59:39 +0100

    toggle menu open/close with full menu title bar

--- src/components/map/layer/map-layer.js
+++ src/components/map/layer/map-layer.js
@@ -180,10 +180,10 @@ class MapLayer extends GestureEventListeners(LitElement) {
           return html ``;
         }
         return html`
-        <div class="mlcontainer">
+        <div class="mlcontainer" @click="${()=>this._toggleArrow()}">
             <div class="mltitle${this.itemcontainer?' draghandle':''}${this.outzoomrange || this.layer.metadata.layervisible === false || this.boundspos !== ""?' lightgray':''}">
                 <base-checkbox ?disabled="${this.outzoomrange || this.boundspos!==''}" ?checked="${this.visible}" title="${ifDefined(t('toggle layer visibility')??undefined)}" @change="${(e)=>this._toggleVisibility(e)}"></base-checkbox>
-                <span @click="${()=>this._toggleArrow()}">${this.layer.metadata?this.layer.metadata.title?this.layer.metadata.title:this.layer.id:this.layer.id}</span>
+                <span>${this.layer.metadata?this.layer.metadata.title?this.layer.metadata.title:this.layer.id:this.layer.id}</span>
                 <base-arrow ?open="${this.open}" @change="${e=>this._openChange(e)}"></base-arrow>
             </div>
             <div class="mlsubtitle">${this.subtitle}</div>
