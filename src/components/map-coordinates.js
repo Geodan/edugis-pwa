458f23d1a8a9789032e5a42749c3fef6ac3033ee
Anne Blankert <anne.blankert@geodan.nl>
2024-04-29 00:54:11 +0200

    lon lat => lat lon, add N,S,E,W and translations

--- src/components/map-coordinates.js
+++ src/components/map-coordinates.js
@@ -1,4 +1,5 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, css} from 'lit';
+import {translate as t, registerLanguageChangedListener, unregisterLanguageChangedListener} from '../i18n.js';
 
 /* polyfill */
 Math.log10 = Math.log10 || function(x) {
@@ -11,14 +12,30 @@ Math.log10 = Math.log10 || function(x) {
 * @extends HTMLElement
 */
 class MapCoordinates extends LitElement {
+  static styles = css`
+    :host {
+      background-color: rgba(255, 255, 255, 0.75);
+      font-size: 12px;
+      position: absolute;
+      display: inline-block;
+      width: 16em;
+      bottom: 10px;
+      left: 50%;
+      margin-left: -10em;
+      text-align: center;
+      border-radius: 4px;
+      box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.1);
+    }
+  `;
   static get properties() { 
     return { 
       visible: Boolean,
-      lat: Number,
-      lon: Number,
-      resolution: Number,
-      clickpoint: Array,
-      copiedcoordinate: String
+      lat: {type: Number},
+      lon: {type: Number},
+      resolution: {type: Number},
+      clickpoint: {type: Array},
+      copiedcoordinate: {type: String},
+      clickpointHtml: {type: Object, attribute: false}
     }; 
   }
   constructor() {
@@ -32,10 +49,34 @@ class MapCoordinates extends LitElement {
     this.resolution = 0.0;
     this.clickpoint = [];
     this.copiedcoordinate = '';
+    this.West = 'W';
+    this.East = 'E';
+    this.North = 'N';
+    this.South = 'S';
+  }
+  initNorthEastSouthWest() {
+    this.West = t('W');
+    this.East = t('E');
+    this.North = t('N');
+    this.South = t('S');
+  }
+  connectedCallback() {
+    super.connectedCallback()
+    this.languageChanged = this.languageChanged.bind(this);
+    registerLanguageChangedListener(this.languageChanged);
+    this.initNorthEastSouthWest();
+  }
+  disconnectedCallback() {
+    super.disconnectedCallback()
+    unregisterLanguageChangedListener(this.languageChanged);
+  }
+  languageChanged() {
+    this.initNorthEastSouthWest();
+    this.requestUpdate();
   }
   copyCoords(e) {
     const copy = this.shadowRoot.querySelector('#copied');
-    this.copiedcoordinate = `${this.clickpoint[0].toFixed(this.factor)},${this.clickpoint[1].toFixed(this.factor)}`;
+    this.copiedcoordinate = `${this.clickpoint[1].toFixed(this.factor)},${this.clickpoint[0].toFixed(this.factor)}`;
     copy.innerHTML = this.copiedcoordinate;
     window.getSelection().removeAllRanges();
     const range = document.createRange();
@@ -50,14 +91,16 @@ class MapCoordinates extends LitElement {
         detail: this.copiedcoordinate
     }))
   }
-  shouldUpdate(changedProps) {
+  shouldUpdate(_changedProps) {
+    return this.visible;
+  }
+  updated(changedProps) {
     if (changedProps.has('resolution')) {
       if (this.resolution > 0) {
         this.factor = -Math.round(Math.log10(this.resolution));
       } else {
         this.factor = 7;
       }
-      return true;
     }    
     if (changedProps.has('clickpoint') && this.clickpoint && this.clickpoint.length === 2) {
       this.clickpointHtml = html`
@@ -83,28 +126,31 @@ class MapCoordinates extends LitElement {
             opacity: 1;
           }
         </style>        
-        <div id="clickpoint" @click="${e=>this.copyCoords(e)}"><span class="ul">Click</span>: ${this.clickpoint[0].toFixed(this.factor)}&deg;&#x2194;&nbsp;&nbsp;${this.clickpoint[1].toFixed(this.factor)}&deg;&#x2195</div>
+        <div id="clickpoint" 
+          @click="${e=>this.copyCoords(e)}"><span class="ul">Click</span>: ${this.latLongString([this.clickpoint[0],this.clickpoint[1]])}</div>
         <div id="copied">Copied!</div>`;
     }
-    return this.visible;
+  }
+  latLongString(point) {
+    const lon = parseFloat(point[0].toFixed(this.factor));
+    let longitude = `${lon}°`
+    if (lon > -180 && lon < 0) {
+      longitude = `${longitude}${this.West}`;
+    } else if (lon > 0 && lon < 180) {
+      longitude=`${longitude}${this.East}`;
+    }
+    const lat = parseFloat(point[1].toFixed(this.factor));
+    let latitude = `${lat}°`;
+    if (lat > -90 && lat < 0) {
+      latitude = `${latitude}${this.South}`;
+    } else if (lat > 0 && lat < 90) {
+      latitude = `${latitude}${this.North}`;
+    }
+    return `${latitude} ${longitude}`;
   }
   render() {
-    return html`<style>
-      :host {
-        background-color: rgba(255, 255, 255, 0.75);
-        font-size: 12px;
-        position: absolute;
-        display: inline-block;
-        width: 16em;
-        bottom: 10px;
-        left: 50%;
-        margin-left: -10em;
-        text-align: center;
-        border-radius: 4px;
-        box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.1);
-      }
-    </style>
-    ${this.lon.toFixed(this.factor)}&deg;&#x2194;&nbsp;&nbsp;${this.lat.toFixed(this.factor)}&deg;&#x2195;
+    return html`
+      <div>${this.latLongString([this.lon,this.lat])}</div>
     ${this.clickpointHtml}
     `
   }
